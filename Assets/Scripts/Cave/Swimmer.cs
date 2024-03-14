39f6892f7ed7484f744adc195bb67726fc25d2b6
projectgild <137822248+projectgild@users.noreply.github.com>
2024-03-14 01:29:07 -0400

    added basic swimming

--- Assets/Scripts/Cave/Swimmer.cs
+++ Assets/Scripts/Cave/Swimmer.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Swimmer : MonoBehaviour
 {
 	[SerializeField]
-	private float _swimSpeed = 5;
+	private float _swimSpeed = 2;
+
 	
 	private float _verticalSwim = 0;
 	private float _sideSwim = 0;
 	private float _swimForward = 0;
+	private float _roll = 0;
 	
-	private Rigidbody _rb;
+	private Rigidbody rb;
+	
+	public int oxyCount = 100;
+	public TMP_Text oxyDisplay;
 	
     // Start is called before the first frame update
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+	rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -24,11 +30,31 @@ public class Swimmer : MonoBehaviour
     {
         _verticalSwim = Input.GetAxis("Swim Up/Down");
 		_sideSwim = Input.GetAxis("Swim Left/Right");
-		_swimForward = Input.GetAxis("Swim Forward") * _swimSpeed;	
+		_swimForward = Input.GetAxis("Swim Forward") * _swimSpeed;
+		_roll = Input.GetAxis("Barrel Roll Left/Right");
+		
+		/*
+		Debug.Log(_verticalSwim);
+		Debug.Log(_sideSwim);
+		Debug.Log(_swimForward);
+		*/
+		
+		swim(_verticalSwim, _sideSwim, _swimForward, _roll);
+		
+		//Invoke("breathe", 1);
     }
 	
-	private void swim(float verticalSwimPower, float sideSwimPower, float swimForwardPower)
+	private void swim(float verticalSwimPower, float sideSwimPower, float swimForwardPower, float rollPower)
 	{
-		_rb.AddForce(new Vector3(0, 1 * _swimSpeed, 0));
+		rb.AddRelativeForce(swimForwardPower * Time.deltaTime * 10, 0, 0);
+		
+		rb.AddRelativeTorque(-0.05f * rollPower, sideSwimPower * Time.deltaTime * 100, verticalSwimPower * Time.deltaTime * 100);
 	}
+
+	private void breathe()
+	{
+		oxyCount -= 1;
+		oxyDisplay.SetText(oxyCount.ToString());
+	}
+	
 }
