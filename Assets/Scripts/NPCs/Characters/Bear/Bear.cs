f9288a037adbeef9794b571f905c5b11a7ecb6c0
Eric Brown <eric.brown8787@gmail.com>
2024-03-28 12:36:53 -0400

    Add fields for sprite display states to bear script

--- Assets/Scripts/NPCs/Characters/Bear/Bear.cs
+++ Assets/Scripts/NPCs/Characters/Bear/Bear.cs
@@ -2,15 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BearState
+{
+    calm,
+    angry,
+    sleeping,
+    walking,
+}
 public class Bear : Character
 {
     [SerializeField] List<Item> inventory;
-    [SerializeField] Item desiredItem;
     [TextArea][SerializeField] string satisfiedDialogue;
     [SerializeField] GameEventTrigger openGateTrigger;
     [SerializeField] GameObject stumpWaypoint;
     [SerializeField] AudioClip[] calmInteractSounds;
 
+    [Header("Sprite Display States")]
+    [SerializeField] BearState state;
+    [SerializeField] Sprite calmSprite;
+    [SerializeField] Sprite angrySprite;
+    [SerializeField] Sprite sleepingSprite;
+    [SerializeField] Sprite walkingSprite;
+
+    private void LateUpdate()
+    {
+        if (state == BearState.walking && transform.position == stumpWaypoint.transform.position) ;
+        {
+            state = BearState.calm;            
+        }
+    }
 
     public override void CharacterBehavior()
     {
