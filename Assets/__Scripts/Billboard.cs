b424a18622998328a1455cc911a744014cc9670f
MegaMagicarp <66321765+MegaMagicarp@users.noreply.github.com>
2024-03-28 11:52:26 -0400

    made a basic billboard script

---
+++ Assets/__Scripts/Billboard.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billbord : MonoBehaviour
+{
+    Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 newRotation = mainCamera.transform.eulerAngles;
+
+        newRotation.x = 0;
+        newRotation.z = 0;
+
+        transform.eulerAngles = newRotation;
+    }
+}
