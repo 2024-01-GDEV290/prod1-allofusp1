41490f59b0c8ea072e04de98d158048238aba0ad
projectgild <137822248+projectgild@users.noreply.github.com>
2024-04-04 08:17:31 -0400

    Added lvl2 and 3 blockouts

--- Assets/__Scripts/Cave/Swimmer.cs
+++ Assets/__Scripts/Cave/Swimmer.cs
@@ -22,10 +22,6 @@ public class Swimmer : MonoBehaviour
 	// The 'breathe' coroutine is used to track the player's oxygen level underwater. They have 100 seconds without oxygen.
 	private Coroutine breathe;
 	
-	
-	// Camera is not a child of the swimmer, but is in a fixed position relative to swimmer
-	public GameObject camera;
-	
     // Start is called before the first frame update
     void Start()
     {
@@ -41,33 +37,35 @@ public class Swimmer : MonoBehaviour
 		_sideSwim = Input.GetAxis("Swim Left/Right");
 		_swimForward = Input.GetAxis("Swim Forward") * _swimSpeed;
 		_roll = Input.GetAxis("Barrel Roll Left/Right");
-		
-		/*
-		Debug.Log(_verticalSwim);
-		Debug.Log(_sideSwim);
-		Debug.Log(_swimForward);
-		*/
-		
+
 		swim(_verticalSwim, _sideSwim, _swimForward, _roll);
-		
-		cameraUpdate(camera);
     }
 	
 	private void swim(float verticalSwimPower, float sideSwimPower, float swimForwardPower, float rollPower)
 	{
+		// Pushes the diver forward
 		rb.AddRelativeForce(swimForwardPower * Time.deltaTime * 10, 0, 0);
 		
+		// Rotates and rolls the diver
 		rb.AddRelativeTorque(-15f * Time.deltaTime * rollPower, sideSwimPower * Time.deltaTime * 100, verticalSwimPower * Time.deltaTime * 100);
 	}
 
+	/*
+	Currently unused, since I can't get it work yet.
 	private void cameraUpdate(GameObject camera)
 	{
+		// Holds the diver's current position
 		Vector3 currentPosition = transform.position;
-		camera.transform.position = new Vector3(currentPosition.x - 3, currentPosition.y, currentPosition.z);
 		
-		camera.transform.rotation = transform.rotation;
-		camera.transform.Rotate(0, 90, 0);
+		// Sets the camera at a position close to, but just behind, the diver
+		camera.transform.position = new Vector3(currentPosition.x, currentPosition.y, currentPosition.z + 2);
+		
+		// Rotates the camera around the diver (in theory)
+		camera.transform.RotateAround(currentPosition, 
+
 	}
+	
+	*/
 
 
 	IEnumerator breathe_time()
