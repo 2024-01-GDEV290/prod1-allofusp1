56f55f86627f530514e0fad6ee2795490387d747
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2024-04-06 14:29:56 -0400

    Add hunter voice and dialogue.

--- Assets/Prefabs/NPCs/Hunter/Hunter.cs
+++ Assets/Prefabs/NPCs/Hunter/Hunter.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class Hunter : Character
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    [SerializeField] string[] defaultLines;
+    
     public override void CharacterBehavior()
     {
-        Debug.Log("Hunter Behavior");
+        InitiateDialogue(defaultLines);
     }
 }
