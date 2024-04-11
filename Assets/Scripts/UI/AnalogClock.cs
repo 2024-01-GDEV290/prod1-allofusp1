a486125e2d0d1d71141f5eb43f18d381f1535b16
ArtemisMacDuffie <122636675+ArtemisMacDuffie@users.noreply.github.com>
2024-04-11 10:10:36 -0400

    hr hand animation polished

--- Assets/Scripts/UI/AnalogClock.cs
+++ Assets/Scripts/UI/AnalogClock.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public class AnalogClock : MonoBehaviour
 {
-    [SerializeField] [Range(0f, 0.1f)] float hrHandDelay;
-
     private void Start()
     {
         UpdateClock();
@@ -15,9 +13,13 @@ public class AnalogClock : MonoBehaviour
     public void UpdateClock()
     {
         float currentHourAngle = this.GetComponent<RectTransform>().localEulerAngles.z;
+        if (currentHourAngle > 0) { currentHourAngle -= 360; }
+
         int newHour = WindingTime.S.hours;
-        if (newHour >= 12) { newHour -= 12; }
-        float newHourAngle = -360 * newHour / 12f + 360;
+        if (newHour > 12) { newHour -= 12; }
+        else if (newHour == 0) { newHour = 12; }
+
+        float newHourAngle = -360 * newHour / 12f;
         //if (newHourAngle <= -360) { newHourAngle += 360; }
 
         StartCoroutine(MoveHrHand(currentHourAngle, newHourAngle));
@@ -33,7 +35,7 @@ public class AnalogClock : MonoBehaviour
         {
             currentHourAngle--;
             this.GetComponent<RectTransform>().localEulerAngles = new Vector3(0f, 0f, currentHourAngle);
-            yield return new WaitForSeconds(hrHandDelay);
+            yield return new WaitForSeconds(1/1000f);
         }
 
         this.GetComponent<RectTransform>().localEulerAngles = new Vector3(0f, 0f, newHourAngle);
