2dd0215a42731af167c3595832f126a79919f3b2
projectgild <137822248+projectgild@users.noreply.github.com>
2024-04-13 01:50:00 -0400

    Updated Cave 2 textures, lighting, controls, fauna

--- Assets/__Scripts/Billboard Tall.cs
+++ Assets/__Scripts/Billboard Tall.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Billbord : MonoBehaviour
 {
-    Camera mainCamera;
+    [SerializeField] Camera mainCamera;
 
     void OnEnable()
     {
@@ -16,7 +16,7 @@ public class Billbord : MonoBehaviour
 
     void Start()
     {
-        mainCamera = Camera.main;
+        //mainCamera = Camera.main;
     }
 
     void LateUpdate()
